d9c473a7815c3a8cac00c341c1922d0ddaaaf1fa
JSaboyaR <jhon.saboya.r@gmail.com>
2021-11-12 17:38:16 -0500

    Complemento
    Se agregan las posibles rutas

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config['SECRET_KEY']=os.urandom(32)
+
 from app import rutas
 
 
--- app/rutas.py
+++ app/rutas.py
@@ -7,6 +7,113 @@ from markupsafe import escape
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+
+#Inicio
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def inicio():
+    var = "inicio"
+    return render_template('prueba.html', var=var)
+
+@app.route('/login')
+def login():
+    var = "login"
+    return render_template('prueba.html', var=var)
+
+@app.route('/recuperar')
+def recuperar():
+    var = "recuperar contraseña"
+    return render_template('prueba.html', var=var)
+
+@app.route('/reestablecer')
+def reestablecer():
+    var = "reestablecer contraseña"
+    return render_template('prueba.html', var=var)
+
+@app.route('/registro')
+def registro():
+    var = "registro externo"
+    return render_template('prueba.html', var=var)
+
+@app.route('/buscar')
+def buscar():
+    var = "buscar producto"
+    return render_template('prueba.html', var=var)
+
+
+# Usuario
+
+@app.route('/user/verproducto')
+def verproducto():
+    var = "ver el producto  "
+    return render_template('prueba.html', var=var)
+
+@app.route('/user/listaf')
+def listaf():
+    var = "lista de compra y deseos"
+    return render_template('prueba.html', var=var)
+
+@app.route('/user/listadeseos')
+def listadeseo():
+    var = "lista de deseos"
+    return render_template('prueba.html', var=var)
+
+@app.route('/user/listacompras')
+def listacompra():
+    var = "lista de compras"
+    return render_template('prueba.html', var=var)
+
+
+# Admin
+
+@app.route('/admin/home')
+def adminhome():
+    var = "admin home"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/registro')
+def adminregistro():
+    var = "admin registro"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/productos')
+def adminproductos():
+    var = "admin productos"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/productos-gestion')
+def adminproductosgestion():
+    var = "admin productos actualizar o eliminar"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/productos-agregar')
+def adminproductosagregar():
+    var = "admin agregar productos"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/clientes')
+def adminclientes():
+    var = "admin clientes"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/usuarios')
+def adminusuarios():
+    var = "admin usuarios"
+    return render_template('prueba.html', var=var)
+
+@app.route('/admin/listados-clientes')
+def adminusuarios():
+    var = "admin listados de clientes"
+    return render_template('prueba.html', var=var)
+
+# Error
+
+@app.errorhandler(404)
+def error404(error):
+    try:
+        var = "error 404"
+        return render_template('prueba.html', var=var)
+    except:
+        var = "error fatal"
+        return render_template('prueba.html', var=var)
+
