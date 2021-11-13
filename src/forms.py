357e29e5cdab34790e1f3519ce5b2968a34c8203
JSaboyaR <jhon.saboya.r@gmail.com>
2021-11-13 00:40:33 -0500

    Delete forms.py

--- src/forms.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, IntegerField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
-
